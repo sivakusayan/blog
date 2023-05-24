521a7e7da58b28612f62eefe9cdfa48a71826ec1
Sayan Sivakumaran <sivakusayan@gmail.com>
2023-05-24 13:53:26 -0500

    First attempt at better handling of delayed theme changes

--- scripts/lightDarkTheme.js
+++ scripts/lightDarkTheme.js
@@ -3,7 +3,8 @@ const darkThemeRadio = document.getElementById("dark-theme-radio");
 const systemThemeRadio = document.getElementById("system-theme-radio");
 let currentCheckedRadio = null;
 let currentTimeoutId = null;
-let selectionFromKeyboard = false;
+let recentlyChanged = false;
+let recentlyChangeTimeoutId = null;
 
 const Themes = Object.freeze({
   LIGHT: "LIGHT",
@@ -15,51 +16,33 @@ const initializeEventListeners = () => {
   lightThemeRadio.onchange = (e) => onThemeRadioChecked(Themes.LIGHT);
   darkThemeRadio.onchange = (e) => onThemeRadioChecked(Themes.DARK);
   systemThemeRadio.onchange = (e) => onThemeRadioChecked(Themes.SYSTEM);
-
-  // Set this flag so we know if the theme change should be delayed or not.
-  // See `onThemeRadioChecked()` for why we need to do this.
-  // Assistive tech interactions should register as clicks if they
-  // use the standard APIs, so they likely won't go through here.
-  // Regardless, it seems like we only need delay for the keyboard anyway.
-  document.getElementById("theme-picker").onkeydown = (e) => {
-    switch (e.key) {
-      case "ArrowDown":
-      case "ArrowUp":
-      case "ArrowLeft":
-      case "ArrowRight":
-        selectionFromKeyboard = true;
-        break;
-      default:
-        break;
-    }
-  };
 };
 
 const onThemeRadioChecked = (theme) => {
   // Only set theme on a timeout so if a user quickly arrows through
   // the theme picker the theme switch isn't jarring and flashing 
   // in their face.
-  if (selectionFromKeyboard) {
+  if (recentlyChanged) {
     setTheme(theme, 500);
   }
   else {
     setTheme(theme);
   }
-
-  // Clear flag for the next time round
-  selectionFromKeyboard = false;
 }
 
 const setTheme = (theme, delay) => {
   updateThemePickerSelection(theme)
-  if (delay) {
+  if (recentlyChanged) {
     if (currentTimeoutId) {
       clearTimeout(currentTimeoutId);
     }
-    currentTimeoutId = setTimeout(() => setThemeTimeoutCallback(theme), 300);
+    currentTimeoutId = setTimeout(() => {
+      setThemeTimeoutCallback(theme);
+      currentTimeoutId = null;
+    }, 300);
   }
   else {
-    setThemeTimeoutCallback(theme)
+    setThemeTimeoutCallback(theme);
   }
 };
 
@@ -67,6 +50,7 @@ const setTheme = (theme, delay) => {
 const setThemeTimeoutCallback = (theme) => {
   document.documentElement.setAttribute("data-theme", theme)
   localStorage.theme = theme;
+  recentlyChanged = true;
 }
 
 // Updates the UI of the theme picker, but doesn't update the theme itself.
