c1b5b257462cd31ceeb7ebd93381cb15844b7ed6
Sayan Sivakumaran <sivakusayan@gmail.com>
2023-06-08 10:20:30 -0500

    Improve mobile responsiveness with new button based theme picker

--- src/scripts/theming.js
+++ src/scripts/theming.js
@@ -1,6 +1,7 @@
 const lightThemeButton = document.getElementById("light-theme-button");
 const darkThemeButton = document.getElementById("dark-theme-button");
 const systemThemeButton = document.getElementById("system-theme-button");
+let currentToggledButton;
 
 const Themes = Object.freeze({
   LIGHT: "LIGHT",
@@ -11,7 +12,24 @@ const Themes = Object.freeze({
 const setTheme = (theme) => {
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.theme = theme;
-  select.value = theme;
+
+  if (currentToggledButton) {
+    currentToggledButton.setAttribute("aria-pressed", "false");
+  }
+  switch (theme) {
+    case Themes.LIGHT:
+      currentToggledButton = lightThemeButton;
+      break;
+    case Themes.DARK:
+      currentToggledButton = darkThemeButton;
+      break;
+    case Themes.SYSTEM:
+      currentToggledButton = systemThemeButton;
+      break;
+    default:
+      break;
+  }
+  currentToggledButton.setAttribute("aria-pressed", "true");
 };
 
 lightThemeButton.onclick = (e) => setTheme(Themes.LIGHT);
