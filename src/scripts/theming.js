d18e911a06f33d41b4588447e97a020012483eee
Sayan Sivakumaran <sivakusayan@gmail.com>
2023-06-08 09:46:17 -0500

    Improve hover state contrast

--- src/scripts/theming.js
+++ src/scripts/theming.js
@@ -1,9 +1,11 @@
-const select = document.getElementById("theme-select");
+const lightThemeButton = document.getElementById("light-theme-button");
+const darkThemeButton = document.getElementById("dark-theme-button");
+const systemThemeButton = document.getElementById("system-theme-button");
 
 const Themes = Object.freeze({
-  LIGHT: "light",
-  DARK: "dark",
-  SYSTEM: "",
+  LIGHT: "LIGHT",
+  DARK: "DARK",
+  SYSTEM: "SYSTEM",
 });
 
 const setTheme = (theme) => {
@@ -12,5 +14,7 @@ const setTheme = (theme) => {
   select.value = theme;
 };
 
-select.onchange = (e) => setTheme(e.target.value);
+lightThemeButton.onclick = (e) => setTheme(Themes.LIGHT);
+darkThemeButton.onclick = (e) => setTheme(Themes.DARK);
+systemThemeButton.onclick = (e) => setTheme(Themes.SYSTEM);
 if ("theme" in localStorage) setTheme(localStorage.theme);
