423e6dc95211104a59ce7468f0e7271fe88b098b
Sayan Sivakumaran <sivakusayan@gmail.com>
2023-07-13 12:45:24 -0500

    Add contact information on landing page

---
+++ src/scripts/email-encryption.js
@@ -0,0 +1,5 @@
+// Make it harder for bots to scrape our email.
+var encEmail = "c2l2YWt1c2F5YW5AZ21haWwuY29t";
+const emailLink = document.getElementById("contact");
+emailLink.tagName = "a";
+emailLink.setAttribute("href", "mailto:".concat(atob(encEmail)));
