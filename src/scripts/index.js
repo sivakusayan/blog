100e964d7eb3dd8597f980cbbb5a72239bafad4e
Sayan Sivakumaran <sivakusayan@gmail.com>
2024-03-07 18:09:49 -0600

    Add aria-label if code snippet is focusable

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,5 +7,11 @@ const codeElements = document.querySelectorAll("pre");
 for (const code of codeElements) {
     if (code.scrollWidth > code.clientWidth) {
         code.tabIndex = 0;
+        // As soon as it's focusable, we'll unfortunately need to give this
+        // an accname since this element will otherwise have an overly verbose
+        // accname of its contents, which will probably be many, MANY lines of
+        // code.
+        code.ariaLabel = "Code Snippet";
+        code.setAttribute("role", "region");
     }
 }
