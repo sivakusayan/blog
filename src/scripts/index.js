41c37f9b66dfeeba261d7cc65f96ff98221a512f
Sayan Sivakumaran <sivakusayan@gmail.com>
2024-03-15 07:24:53 -0500

    Prototype

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,3 +15,36 @@ for (const code of codeElements) {
 		code.setAttribute('role', 'region');
 	}
 }
+
+var canvas = document.getElementById('canvas');
+if (canvas) {
+	var ctx = canvas.getContext('2d');
+    width = canvas.parentElement.offsetWidth;
+    height = canvas.parentElement.offsetHeight;
+    canvas.width = width + 200;
+    canvas.height = height + 200;
+    maxRadius = 50;
+	var refresh = function () {
+		ctx.clearRect(0, 0, width, height);
+		for (i = 0; i < 20; i++) {
+            var radius = Math.floor(Math.random() * maxRadius);
+			if (radius < maxRadius/3) radius = maxRadius;
+			var x = Math.floor(Math.random() * width);
+            if (x < radius) x = radius;
+            if ((width - x) < radius) x-= radius;
+			var y = Math.floor(Math.random() * height);
+            if (y < radius) y = radius;
+			
+			var r = Math.floor(Math.random() * 100);
+			var g = Math.floor(Math.random() * 150);
+			var b = Math.floor(Math.random() * 255);
+
+			ctx.beginPath();
+			ctx.arc(x, y, radius, Math.PI * 2, 0, false);
+			ctx.fillStyle = 'rgba(' + r + ',' + g + ',' + 255 + ',0.1)';
+			ctx.fill();
+			ctx.closePath();
+		}
+	};
+	refresh();
+}
